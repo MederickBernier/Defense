07542c46210be2369a4f01260e27c5c92725b275
Mederick Bernier <mederick.bernier@hotmail.ca>
2025-09-11 06:43:04 -0400

    Added more functionalities to the Guard

--- src/Core/Guard.php
+++ src/Core/Guard.php
@@ -60,4 +60,56 @@ final class Guard
     {
         self::against($value <= 0, $message);
     }
+
+    /**
+     * Ensures that the given string value is not empty after trimming whitespace.
+     *
+     * @param string $value The string value to check.
+     * @param string $message Optional. The exception message if the value is empty. Defaults to "Value cannot be empty".
+     * @throws \InvalidArgumentException If the string is empty after trimming.
+     */
+    public static function notEmptyString(string $value, string $message = "Value cannot be empty"):void{
+        self::against(trim($value) === '', $message);
+    }
+
+    /**
+     * Validates that the given string does not exceed the specified maximum length.
+     *
+     * @param string $value   The string to check.
+     * @param int    $max     The maximum allowed length.
+     * @param string $message Optional. The exception message if validation fails. Defaults to "String too long".
+     *
+     * @throws \Exception If the string length exceeds the maximum allowed.
+     */
+    public static function maxLength(string $value, int $max, string $message = "String too long"):void{
+     self::against(mb_strlen($value)>$max, $message);   
+    }
+
+    /**
+     * Validates that a given value exists within a specified array.
+     *
+     * @param string|int $needle   The value to search for in the array.
+     * @param array      $haystack The array to search within.
+     * @param string     $message  The exception message to use if validation fails. Defaults to "Invalid value".
+     *
+     * @throws \InvalidArgumentException If the value is not found in the array.
+     */
+    public static function inArray(string|int $needle, array $haystack, string $message = "Invalid value"):void{
+        self::against(!in_array($needle, $haystack, true), $message);
+    }
+
+    /**
+     * Ensures that a given value is greater than a specified minimum.
+     *
+     * Throws an exception with the provided message if the value is less than or equal to the minimum.
+     *
+     * @param int|float $value   The value to check.
+     * @param int|float $min     The minimum value that $value must exceed.
+     * @param string    $message The exception message if the check fails. Defaults to "Too small".
+     *
+     * @throws \Exception If $value is less than or equal to $min.
+     */
+    public static function greaterThan(int|float $value, int|float $min, string $message = "Too small"):void{
+        self::against($value <= $min, $message);
+    }
 }
